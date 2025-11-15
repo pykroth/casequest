e3e464808aa8056d6f9d87882bfff48ca3139b08
pykroth <pyrakroth@gmail.com>
2025-11-15 12:46:27 -0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red'>
+        Hello
+       </div>
+    </>
+  )
+}
+
+export default App
